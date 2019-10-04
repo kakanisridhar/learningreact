4ef2f888a6ce58420012892a32aed9221b8ff855
kakani sridhar <sreedhara.kakani@yahoo.co.uk>
2019-10-04 22:04:52 +0100

    removed unnecessary deps

--- webpack/common.webpack.js
+++ webpack/common.webpack.js
@@ -82,7 +82,7 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader']
+      use: ['style-loader', 'css-loader']
     },
     {
       test: /\.svg$/,
