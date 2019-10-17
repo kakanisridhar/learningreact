a79316e155a018775e2220bf9ef29e82db0d570d
kakanis <sreedhara.kakani@equiniti.com>
2019-10-17 13:28:29 +0100

    fix browser history

--- src/screens/ReduxSaga/store/index.js
+++ src/screens/ReduxSaga/store/index.js
@@ -1,34 +1,26 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { routerMiddleware } from 'connected-react-router'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'connected-react-router';
 import createSagaMiddleware from 'redux-saga';
-import createHistory from 'history/createBrowserHistory'
-import rootReducer from '../rootReducer'
+import { createBrowserHistory } from 'history';
+import rootReducer from '../rootReducer';
 import rootSaga from '../sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-export const history = createHistory()
+export const history = createBrowserHistory();
 
-const initialState = {}
-const enhancers = []
-const middleware = [sagaMiddleware, routerMiddleware(history)]
-
-if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
-
-  if (typeof devToolsExtension === 'function') {
-    enhancers.push(devToolsExtension())
-  }
-}
+const initialState = {};
+const enhancers = [];
+const middleware = [sagaMiddleware, routerMiddleware(history)];
 
 const composedEnhancers = compose(
   applyMiddleware(...middleware),
   ...enhancers
-)
+);
 
 export default createStore(
-    rootReducer(history),
-    initialState,
-    composedEnhancers
-)
+  rootReducer(history),
+  initialState,
+  composedEnhancers
+);
 
 sagaMiddleware.run(rootSaga);
