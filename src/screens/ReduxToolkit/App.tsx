dc241d08837a4144f970661c72e0630a01ab7834
skakani <sreedhara.kakani@homeoffice.gov.uk>
2020-10-08 20:10:00 +0100

    redux toolkit

---
+++ src/screens/ReduxToolkit/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Switch from 'react-bootstrap/esm/Switch';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Container from '../Shared/components/Container';
+import Navbar from '../Shared/components/Navbar';
+
+export default function App() {
+    return (
+        <Router>
+            <Container>
+                <Navbar>
+                    <div>
+                        <Link to="/Lesson1">Slice</Link>
+                    </div>
+                    <div>
+                        <Link to="/Lesson2">Thunks</Link>
+                    </div>
+                    <div>
+                        <Link to="/Lesson3">Entity Adapters</Link>
+                    </div>
+                </Navbar>
+                <Switch>
+                    <Route path="*">
+                        Learning Redux toolkit
+                    </Route>
+                </Switch>
+            </Container>
+        </Router>
+    );
+}
